e85152b7c8ac9414667bc3836cf71d7b91dda04b
Michal G <gawlas05@gmail.com>
2024-12-12 18:05:53 +0100

    Adding home route tests

---
+++ src/routes/HomeRoute.test.js
@@ -0,0 +1,5 @@
+import { render, screen } from "@testing-library/react";
+import { setupServer } from "msw/node";
+import { rest } from "msw";
+import { MemoryRouter } from "react-router";
+import HomeRoute from "./HomeRoute";
